e6a0a3dbe564b654b20e1229ec245953e53f3611
boy0501 <boy0501@naver.com>
2023-02-12 21:30:36 +0900

    백준 1541 잃어버린괄호 완

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -1,29 +1,73 @@
+// main.cpp
+
 #include <iostream>
-#include <string>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <cstring>
+#include <stack>
+#include <map>
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(nullptr);
-	string str; string tmp; int result = 0;
-	bool check = false;
-	cin >> str;
-	for (int i = 0; i <= str.length(); i++) {
-		if (str[i] != '-' && str[i] != '+' && i != str.length()) {
-			tmp += str[i];
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	string tmp;
+	cin >> tmp;
+
+	int left{}, right{};
+
+	int cntNum = 0;
+	int Separator = 0;
+	bool minustrig = false;
+	bool first = false;
+	for (auto c : tmp)
+	{
+		if (c == '+')
+		{
+			string num{ tmp.begin() + Separator, tmp.begin() + cntNum };
+			int cal = atoi(num.c_str());
+			right += cal;
+			Separator = ++cntNum;
 		}
-		else {
-			if (!check) {
-				result += stoi(tmp, nullptr, 10);
-				tmp.clear();
+		else if (c == '-')
+		{
+			minustrig = true;
+			string num{ tmp.begin() + Separator, tmp.begin()+cntNum };
+			int cal = atoi(num.c_str());
+			right += cal;
+			if (first == 0)
+			{
+				left = right;
+				right = 0;
+				first = true;
 			}
 			else {
-				result -= stoi(tmp, nullptr, 10);
-				tmp.clear();
+				left -= right;
+				right = 0;
 			}
+			Separator = ++cntNum;
 		}
-
-		if (str[i] == '-')
-			check = true;
+		else {
+			cntNum++;
+		}
 	}
-	cout << result;
+
+	string num{ tmp.begin() + Separator, tmp.begin() + cntNum };
+	int cal = atoi(num.c_str());
+	right += cal;
+
+	if (minustrig)
+	{
+		left -= right;
+	}
+	else {
+		left += right;
+	}
+	cout << left;
+
+
+	return 0;
 }
