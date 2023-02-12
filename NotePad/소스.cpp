5ab5d7920e1b00e9bdaf1e7d8601c1103edf6cd9
boy0501 <boy0501@naver.com>
2023-02-12 21:00:54 +0900

    백준 1541번 잃어버린 괄호 완

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -1,49 +1,29 @@
-// main.cpp
-
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-#include <cstring>
-#include <stack>
-#include <map>
+#include <string>
 using namespace std;
 
-int main()
-{
-	std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);
-
-	int N, M;
-	cin >> N >> M;
-	map<string, int> dbojob;
-	for (int i = 0; i < N; ++i)
-	{
-		string tmp;
-		cin >> tmp;
-		dbojob[tmp]++;
-	}
-	for (int i = 0; i < M; ++i)
-	{
-		string tmp;
-		cin >> tmp;
-		dbojob[tmp]++;
-	}
-	int res = 0;
-	vector<string> people;
-
-	for (auto& person : dbojob)
-	{
-		if (person.second == 2)
-		{
-			res++;
-			people.push_back(person.first);
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	string str; string tmp; int result = 0;
+	bool check = false;
+	cin >> str;
+	for (int i = 0; i <= str.length(); i++) {
+		if (str[i] != '-' && str[i] != '+' && i != str.length()) {
+			tmp += str[i];
 		}
+		else {
+			if (!check) {
+				result += stoi(tmp, nullptr, 10);
+				tmp.clear();
+			}
+			else {
+				result -= stoi(tmp, nullptr, 10);
+				tmp.clear();
+			}
+		}
+
+		if (str[i] == '-')
+			check = true;
 	}
-	cout << res << endl;
-	for (auto& person : people)
-	{
-		cout << person << endl;
-	}
-	return 0;
+	cout << result;
 }
