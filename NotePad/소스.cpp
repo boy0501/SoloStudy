c15aedd6564ea294f36fb2cf05a949dec1de2993
boy0501 <boy0501@naver.com>
2023-02-14 21:30:52 +0900

    7576번 골드5 토마토 완
    BFS문제

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -11,36 +11,86 @@
 #include <cmath>
 using namespace std;
 
+int troom[1001][1001];
+
+class Pos {
+public:
+	int x, y;
+	Pos() {};
+	Pos(int xx, int yy) : x(xx), y(yy) {}
+};
+int dx[4] = { 0 , 0 , 1 ,-1 };
+int dy[4] = { 1 ,-1, 0,0 };
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	string num;
-	cin >> num;
+	int N, M;
+	cin >> N >> M;
 
-	int digit = num.length();
-
-	int value = atoi(num.c_str());
-	int ex = value - (digit * 9);
-	int res = 1'111'111;
-	
-	for (int i = ex; i < value; ++i)
+	memset(troom, - 1, sizeof(troom));
+	vector<Pos> vec;
+	for (int i = 1; i <= M; ++i)
 	{
-		string tmp;
-		tmp = to_string(i);
-		int k = i;
-		for (auto c : tmp)
+		for (int j = 1; j <= N; ++j)
 		{
-			k += c - '0';
-		}
-		if (value == k)
-		{
-			res = min(res, i);
+			cin >> troom[i][j];
+			if (troom[i][j] == 1)
+			{
+				vec.push_back(Pos(j, i));
+			}
 		}
 	}
-	if (res == 1'111'111)
-		cout << 0;
-	cout << res;
+
+
+	queue<pair<Pos, int>> q;
+	
+	for (auto v : vec)
+	{
+		q.push(make_pair(v, 0));
+	}
+	int maxstep = 0;
+
+	while (!q.empty())
+	{
+		auto spot = q.front();
+		q.pop();
+		for (int i = 0; i < 4; ++i)
+		{
+			int nx = spot.first.x;
+			int ny = spot.first.y;
+			nx += dx[i];
+			ny += dy[i];
+
+			if (troom[ny][nx] == 0)
+			{
+				troom[ny][nx] = 1;
+				q.push(make_pair(Pos(nx, ny), spot.second + 1));
+				maxstep = max(maxstep, spot.second + 1);
+			}
+		}
+	}
+
+	bool trig = false;
+	for (int i = 1; i <= M; ++i)
+	{
+		for (int j = 1; j <= N; ++j)
+		{
+			if (troom[i][j] == 0)
+			{
+				trig = true;
+			}
+		}
+	}
+
+	if (trig)
+	{
+		cout << -1;
+	}
+	else {
+		cout << maxstep;
+	}
+
 	return 0;
 }
