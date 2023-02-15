6a55a185d22ace8491d9727f08b0aab358b663f7
boy0501 <boy0501@naver.com>
2023-02-15 21:51:01 +0900

    1676 실버5 팩토리얼 0의 개수 완
    수학

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -11,86 +11,49 @@
 #include <cmath>
 using namespace std;
 
-int troom[1001][1001];
-
-class Pos {
-public:
-	int x, y;
-	Pos() {};
-	Pos(int xx, int yy) : x(xx), y(yy) {}
-};
-int dx[4] = { 0 , 0 , 1 ,-1 };
-int dy[4] = { 1 ,-1, 0,0 };
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
+	int n;
+	cin >> n;
 
-	memset(troom, - 1, sizeof(troom));
-	vector<Pos> vec;
-	for (int i = 1; i <= M; ++i)
+	int res = 0;
+	int digit2 = 0;
+	int digit5 = 0;
+	for (int i = 1; i <= n; ++i)
 	{
-		for (int j = 1; j <= N; ++j)
+		if (i % 125 == 0)
 		{
-			cin >> troom[i][j];
-			if (troom[i][j] == 1)
-			{
-				vec.push_back(Pos(j, i));
-			}
+			digit5 += 3;
+		}
+		else if (i % 100 == 0)
+		{
+			res += 2;
+		}
+		else if (i % 25 == 0)
+		{
+			digit5 += 2;
+		}
+		else if (i % 10 == 0)
+		{
+			res += 1;
+		}
+		else if (i % 5 == 0)
+		{
+			digit5++;
+		}
+		else if (i % 2 == 0)
+		{
+			digit2++;
 		}
 	}
 
+	res += min(digit5, digit2);
 
-	queue<pair<Pos, int>> q;
-	
-	for (auto v : vec)
-	{
-		q.push(make_pair(v, 0));
-	}
-	int maxstep = 0;
+	cout << res;
 
-	while (!q.empty())
-	{
-		auto spot = q.front();
-		q.pop();
-		for (int i = 0; i < 4; ++i)
-		{
-			int nx = spot.first.x;
-			int ny = spot.first.y;
-			nx += dx[i];
-			ny += dy[i];
-
-			if (troom[ny][nx] == 0)
-			{
-				troom[ny][nx] = 1;
-				q.push(make_pair(Pos(nx, ny), spot.second + 1));
-				maxstep = max(maxstep, spot.second + 1);
-			}
-		}
-	}
-
-	bool trig = false;
-	for (int i = 1; i <= M; ++i)
-	{
-		for (int j = 1; j <= N; ++j)
-		{
-			if (troom[i][j] == 0)
-			{
-				trig = true;
-			}
-		}
-	}
-
-	if (trig)
-	{
-		cout << -1;
-	}
-	else {
-		cout << maxstep;
-	}
 
 	return 0;
 }
