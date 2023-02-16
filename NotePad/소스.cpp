b0f5ad3b5bd74b408c26671c315946eeea932544
boy0501 <boy0501@naver.com>
2023-02-17 06:23:39 +0900

    1085 브론즈3 직사각형에서 탈출 완
    수학 기하학 문제

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -9,6 +9,7 @@
 #include <stack>
 #include <map>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -16,43 +17,13 @@ int main()
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	
-	int num;
+	int x, y, w, h;
 
-	cin >> num;
+	cin >> x >> y >> w >> h;
 
-	vector<int> vec;
-	unordered_map<int, int> mm;
-	int res = 0;
-	for (int i = 0; i < num; ++i)
-	{
-		int c;
-		cin >> c;
-		vec.push_back(c);
-		res += c;
-		mm[c]++;
-	}
+	int lessw = min(x, w - x);
+	int lessh = min(y, h - y);
 
-	sort(vec.begin(), vec.end());
-	vector<pair<int, int>> kk;
-	for (auto p : mm)
-	{
-		kk.push_back(make_pair(p.second, p.first));
-	}
-	sort(kk.begin(), kk.end(), [](const pair<int,int>& a, const pair<int,int>& b) {
-		if (a.first == b.first)
-			return a.second < b.second;
-		return a.first > b.first;
-	
-	});
-	int manys = kk[0].second;
-	if (kk.size() > 1)
-	{
-		if (kk[0].first == kk[1].first)
-			manys = kk[1].second;
-	}
-	cout << floor(res / float(num) + 0.5) << endl;
-	cout << vec[num/2] << endl;
-	cout << manys << endl;
-	cout << vec[num-1] - vec[0]<< endl;
+	cout << min(lessw, lessh);
 	return 0;
 }
