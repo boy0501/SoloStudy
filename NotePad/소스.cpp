5d5a404611389e5cbe7b858de6ffc42b8c0214af
Spongebang <boy0501@naver.com>
2023-02-22 16:11:57 +0900

    4949번 실버4 균형잡힌 세상 완

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -12,83 +12,65 @@
 #include <algorithm>
 using namespace std;
 
-vector<pair<int, int>> saves;
-
-int checking(int time, int during)
+void check(string str)
 {
-	int cstart = time;
-	int cend = time + during;
-	for (auto& save : saves)
+	stack<char> s;
+	for (auto& c : str)
 	{
-		int sstart = save.first;
-		int send = save.second;
-
-		if (send > cstart && sstart < cstart)
-			return false;
-		if (sstart < cend && cstart < sstart)
-			return false;
-		if (sstart <= cstart && cend <= send)
+		if (c == '(' || c == '[')
 		{
-			if(sstart != send)
-				return false;
+			s.push(c);
 		}
-		if (cstart <= sstart && send <= cend)
+		else if (c == ')')
 		{
-			if (sstart != send)
-				return false;
+			if (s.empty())
+			{
+				cout << "no" << endl;
+				return;
+			}
+			char cc = s.top();
+			if (cc != '(')
+			{
+				cout << "no" << endl;
+				return;
+			}
+			s.pop();
+		}
+		else if (c == ']')
+		{
+			if (s.empty())
+			{
+				cout << "no" << endl;
+				return;
+			}
+			char cc = s.top();
+			if (cc != '[')
+			{
+				cout << "no" << endl;
+				return;
+			}
+			s.pop();
 		}
 	}
-	return true;
+	if (s.empty())
+		cout << "yes" << endl;
+	else
+		cout << "no" << endl;
 }
 
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-	int maxtime = 0;
-	int N;
-	cin >> N;
-	vector<pair<int, int>> objs;
-	for (int i = 0; i < N; ++i)
-	{
-		int a, b;
-		cin >> a >> b;
-		maxtime = max(maxtime, b);
-		objs.push_back(make_pair(a, b - a));
-	}
 
-	vector<pair<int, int>> objsorder{ objs };
-	sort(objs.begin(), objs.end(), [](const pair<int,int>& a, const pair<int,int>& b) {
-		return a.second < b.second;	
-	});
-
-	sort(objsorder.begin(), objsorder.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-		if (a.first == b.first)
-		{
-			return a.first + a.second < b.first + b.second;
-		}
-		else {
-			return a.first < b.first;
-		}
-	});
-
-	for (auto& obj : objs)
+	string str;
+	while (true)
 	{
-		if (checking(obj.first, obj.second))
-		{
-			saves.push_back(make_pair(obj.first, obj.first + obj.second));
-		}
+		getline(cin, str);
+		if (str[0] == '.')
+			break;
+		check(str);
+		str.clear();
 	}
-	int ahigh = saves.size();
-	saves.clear();
-	for (auto& obj : objsorder)
-	{
-		if (checking(obj.first, obj.second))
-		{
-			saves.push_back(make_pair(obj.first, obj.first + obj.second));
-		}
-	}
-	int bhigh = saves.size();
-	cout << max(ahigh,bhigh);
 	return 0;
 }
