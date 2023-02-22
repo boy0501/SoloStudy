e998842e20b788defa3e2e2622a09a02269cfaf2
Spongebang <boy0501@naver.com>
2023-02-22 15:52:03 +0900

    1931 실버1 회의실 배정 실패

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -12,68 +12,83 @@
 #include <algorithm>
 using namespace std;
 
-int maps[2200][2200];
-int res[3];
-void tTree(int Xmin, int Xmax, int Ymin, int Ymax)
+vector<pair<int, int>> saves;
+
+int checking(int time, int during)
 {
-	int gizun = maps[Ymin][Xmin];
-	bool flag = false;
-	for (int i = Ymin; i < Ymax; ++i)
+	int cstart = time;
+	int cend = time + during;
+	for (auto& save : saves)
 	{
-		for (int j = Xmin; j < Xmax; ++j)
+		int sstart = save.first;
+		int send = save.second;
+
+		if (send > cstart && sstart < cstart)
+			return false;
+		if (sstart < cend && cstart < sstart)
+			return false;
+		if (sstart <= cstart && cend <= send)
 		{
-			if (gizun != maps[i][j])
-			{
-				flag = true;
-				goto ids;
-			}
+			if(sstart != send)
+				return false;
+		}
+		if (cstart <= sstart && send <= cend)
+		{
+			if (sstart != send)
+				return false;
 		}
 	}
-
-ids:
-	if (flag)
-	{
-		int t = (Ymax - Ymin ) / 3;
-		tTree(Xmin, Xmin + t, Ymin, Ymin + t);
-		tTree(Xmin + t, Xmin + t + t, Ymin, Ymin + t);
-		tTree(Xmax - t, Xmax, Ymin, Ymin + t);
-
-
-		tTree(Xmin, Xmin + t, Ymin + t,Ymin + t + t);
-		tTree(Xmin +t, Xmin + t + t, Ymin + t, Ymin + t + t);
-		tTree(Xmax - t, Xmax,  Ymin + t, Ymin + t + t);
-
-		tTree(Xmin, Xmin + t, Ymax - t, Ymax);
-		tTree(Xmin + t, Xmin + t + t, Ymax - t, Ymax);
-		tTree(Xmax - t, Xmax, Ymax - t, Ymax);
-	}
-	else {
-		res[gizun + 1]++;
-	}
-
+	return true;
 }
-
 
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-	
+	int maxtime = 0;
 	int N;
 	cin >> N;
-
+	vector<pair<int, int>> objs;
 	for (int i = 0; i < N; ++i)
 	{
-		for (int j = 0; j < N; ++j)
-		{
-			cin >> maps[i][j];
-		}
+		int a, b;
+		cin >> a >> b;
+		maxtime = max(maxtime, b);
+		objs.push_back(make_pair(a, b - a));
 	}
 
-	tTree(0, N,0,N);
+	vector<pair<int, int>> objsorder{ objs };
+	sort(objs.begin(), objs.end(), [](const pair<int,int>& a, const pair<int,int>& b) {
+		return a.second < b.second;	
+	});
 
-	cout << res[0] << endl;
-	cout << res[1] << endl;
-	cout << res[2];
+	sort(objsorder.begin(), objsorder.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+		if (a.first == b.first)
+		{
+			return a.first + a.second < b.first + b.second;
+		}
+		else {
+			return a.first < b.first;
+		}
+	});
+
+	for (auto& obj : objs)
+	{
+		if (checking(obj.first, obj.second))
+		{
+			saves.push_back(make_pair(obj.first, obj.first + obj.second));
+		}
+	}
+	int ahigh = saves.size();
+	saves.clear();
+	for (auto& obj : objsorder)
+	{
+		if (checking(obj.first, obj.second))
+		{
+			saves.push_back(make_pair(obj.first, obj.first + obj.second));
+		}
+	}
+	int bhigh = saves.size();
+	cout << max(ahigh,bhigh);
 	return 0;
 }
