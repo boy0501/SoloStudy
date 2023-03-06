60e88ace794d89d313799c767b6b781b998208e2
boy0501 <boy0501@naver.com>
2023-03-06 13:34:21 +0900

    각 플레이어마다 랜덤난수 동기화 방법
    이 방법을 사용하면 서버와 클라간 난수릉 동기화할 수 있기 때문에, 클라에서 선처리 하여 빠르게 플레이어한테 보여주고, 서버에서는 후처리가 가능한 방식이다.
    
    시드값을 같게 하면 항상 똑같음.

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -9,112 +9,41 @@
 #include <stack>
 #include <map>
 #include <cmath>
+#include <random>
 #include <algorithm>
 using namespace std;
 
-int visit[102][102];
-
-int dx[4]{ 1,-1,0,0 };
-int dy[4]{ 0,0,1,-1 };
-
-class Node {
+class Player {
 public:
-	int H;
-	int x, y;
-	int road;
-	int history{ 0 };
-	Node* prev{nullptr};
-
-	void calcul(int xEnd, int yEnd)
-	{
-		H = (x - xEnd) + (y - yEnd);
-	}
-
-	void calculA()
-	{
-		H += history;
-	}
-
+	mt19937_64 rng;
 };
-struct cmp {
-public:
-	bool operator()(const Node* a, const Node* b)
-	{
-		return a->H > b->H;
-	}
-};
-Node nodes[102][102];
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
-	memset(visit, -1, sizeof(visit));
-	for (int i = 1; i <= N; ++i)
-	{
-		string a;
-		cin >> a;
+	Player pl[3];
+	uniform_int_distribution<int> a(0, 100);
+	pl[0].rng.seed(100);
+	for (int i = 0; i < 20; ++i)
+		cout << a(pl[0].rng) << endl;
 
-		for (int j = 1; j <= M; ++j)
-		{
-			nodes[i][j].road = a[j-1]-'0';
-			nodes[i][j].x = j;
-			nodes[i][j].y = i;
-			nodes[i][j].calcul(M, N);
-			visit[i][j] = 0;
-		}
-	}
+	cout << "------------ p 1 \n";
 
-	queue<Node*> pq;
-	
-	nodes[1][1].history = 1;
-	pq.push(&nodes[1][1]);
-	visit[1][1] = true;
+	pl[1].rng.seed(101);
+	for (int i = 0; i < 20; ++i)
+		cout << a(pl[1].rng) << endl;
+	cout << "------------ p 2 \n";
+	pl[0].rng.seed(100);
+	//srand(100);
+	for (int i = 0; i < 20; ++i)
+		cout << a(pl[0].rng) << endl;
 
-	while (!pq.empty())
-	{
-		auto node = pq.front();
-		pq.pop();
-		if (node->x == M && node->y == N)
-			break;
-		int nX, nY;
-		for (int i = 0; i < 4; ++i)
-		{
-			nX = node->x + dx[i];
-			nY = node->y + dy[i];
+	cout << "------------ p 1 \n";
 
-			if (visit[nY][nX] == 0 && nodes[nY][nX].road == 1)
-			{
-				nodes[nY][nX].prev = node;
-				visit[nY][nX] = true;
-				nodes[nY][nX].history = node->history + 1;
-				nodes[nY][nX].calculA();				
-				pq.push(&nodes[nY][nX]);
-			}
-		}
-
-	}
-
-	int answer = 1;
-	Node* prv = &nodes[N][M];
-	if (prv->prev != nullptr)
-	{
-		while (true)
-		{
-			if (prv->prev->x == 1 && prv->prev->y == 1)
-			{
-				answer++;
-				break;
-			}
-			else {
-				prv = prv->prev;
-				answer++;
-			}
-		}
-	}
-
-	cout << answer;
+	pl[1].rng.seed(101);
+	for (int i = 0; i < 20; ++i)
+		cout << a(pl[1].rng) << endl;
+	cout << "------------ p 2 \n";
 	return 0;
 }
