a407fc24b41f3a215511b9c2c5dd53c03f8f1c8f
boy0501 <boy0501@naver.com>
2023-03-23 22:36:39 +0900

    db공부 조금 SP,PS
    저장프로시저와 준비된명령문에 대해서 조금 공부해봤음
    Mysql을 사용하려다 보니까 이것저것 뭐가 많아서 하나씩 알아가려하는중.

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -17,19 +17,17 @@ class Player {
 public:
 	mt19937_64 rng;
 };
+
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-
 	Player pl[3];
 	uniform_int_distribution<int> a(0, 100);
 	pl[0].rng.seed(100);
 	for (int i = 0; i < 20; ++i)
 		cout << a(pl[0].rng) << endl;
-
 	cout << "------------ p 1 \n";
-
 	pl[1].rng.seed(101);
 	for (int i = 0; i < 20; ++i)
 		cout << a(pl[1].rng) << endl;
@@ -38,9 +36,7 @@ int main()
 	//srand(100);
 	for (int i = 0; i < 20; ++i)
 		cout << a(pl[0].rng) << endl;
-
 	cout << "------------ p 1 \n";
-
 	pl[1].rng.seed(101);
 	for (int i = 0; i < 20; ++i)
 		cout << a(pl[1].rng) << endl;
