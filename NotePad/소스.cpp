54c125caeef8b83bb5e1a6d4795434cfd289f995
boy0501 <boy0501@naver.com>
2023-06-01 21:00:35 +0900

    매크로/inline 간단공부

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -18,28 +18,15 @@ public:
 	mt19937_64 rng;
 };
 
+#define TESTFUNC(x,y) cout << #y#x <<"입니다"<<y<<"입니다\n"
+
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-	Player pl[3];
-	uniform_int_distribution<int> a(0, 100);
-	pl[0].rng.seed(100);
-	for (int i = 0; i < 20; ++i)
-		cout << a(pl[0].rng) << endl;
-	cout << "------------ p 1 \n";
-	pl[1].rng.seed(101);
-	for (int i = 0; i < 20; ++i)
-		cout << a(pl[1].rng) << endl;
-	cout << "------------ p 2 \n";
-	pl[0].rng.seed(100);
-	//srand(100);
-	for (int i = 0; i < 20; ++i)
-		cout << a(pl[0].rng) << endl;
-	cout << "------------ p 1 \n";
-	pl[1].rng.seed(101);
-	for (int i = 0; i < 20; ++i)
-		cout << a(pl[1].rng) << endl;
-	cout << "------------ p 2 \n";
+	double skill = 0;
+
+	TESTFUNC(1, 2);
+
 	return 0;
 }
