b0f6de0910c34128c84f2993a195323109400cbf
boy0501 <boy0501@naver.com>
2023-06-16 04:37:42 +0900

    16953 실버2 A -> B 완
    간단한 dfs or bfs 문제
    나 같은 경우 작은 수에서 큰 수로 늘려가며 찾았는데 이렇게 될 경우, 조건에서 overflow가 일어날 가능성이 있다. 그래서 int 타입이 아닌 long long으로 해주어야 통과, 높은수에서 아랫수로 가는게 더 효율적이라고 생각이 된다.

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -20,94 +20,29 @@ public:
 };
 #define inf 100000001
 
+int dfs(int start,int end,int level)
+{
+	if (start == end) return level;
+	if (start > end) return 0;
+	int res = 0;
+	res += dfs(start * 2, end, level+1);
+	res += dfs(start * 10 + 1, end, level+ 1);
+
+	return res;
+}
 
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	int n, m;
-	cin >> n >> m;
-	int pcnt;
-	unordered_map<int, int> truth;
-	vector<pair<vector<int>,int>> parties;
-	cin >> pcnt;
-	for (int i = 0; i < pcnt; ++i)
-	{
-		int t;
-		cin >> t;
-		truth[t] = 1;
-	}
-	int res = 0;
-	int affected = false;
-	for (int i = 0; i < m; ++i)
-	{
-		int cnt;
-		bool JustSpeakTrue = false;
-		cin >> cnt;
-		vector<int> party;
-		for (int j = 0; j < cnt; ++j)
-		{
-			int person;
-			cin >> person;
-			if (truth[person] > 0)
-			{
-				affected = true;
-				JustSpeakTrue = true;
-			}
-			party.push_back(person);
-		}
-		parties.push_back(make_pair(party, JustSpeakTrue));	//진실을 말해야하면 영향받는 사람갯수 - cnt 
-	}
+	int start,end;
+	cin >> start >> end;
+	int res = dfs(start, end, 0);
+	if (res == 0)
+		cout << -1;
+	else
+		cout << res + 1;
 
-	int partynum = 0;
-	while (affected && partynum != m)
-	{
-		partynum++;
-		affected = false;
-		//정렬 
-		// 기준 1. 거짓을 말해도 되는 순 
-		// 기준 2. 영향 받는 사람의 수가 높은 순
-		sort(parties.begin(), parties.end(), [](const pair<vector<int>, int>& a, const pair<vector<int>, int>& b) {
-			if (b.second == a.second)
-			{
-				return b.first.size() < a.first.size();
-			}
-			else {
-				return b.second < a.second;
-			}
-		});
-
-		for (auto& party : parties)
-		{
-			for (auto& person : party.first)
-			{
-				if (truth[person] > 0)
-				{
-					affected = true;	//영향을 받았으니 한번 더 검사
-					for (auto& per : party.first)
-					{
-						truth[per] = 1;
-					}
-					break;
-				}
-			}
-		}
-	}
-
-	int truthparty = 0;
-	for (auto& party : parties)
-	{
-		for (auto& person : party.first)
-		{
-			if (truth[person] > 0)
-			{
-				truthparty++;
-				break;
-			}
-		}
-	}
-
-	cout << m - truthparty << endl;
 	return 0;
 }
