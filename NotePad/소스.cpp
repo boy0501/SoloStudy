d2c39f4bcb96eb3a5a2fdedfc88b912928b78a72
boy0501 <boy0501@naver.com>
2023-09-09 02:05:29 +0900

    프로그래머스 2단계문제
    https://school.programmers.co.kr/learn/courses/30/lessons/12914
    
    아니 dp로 풀고 다시보니까 이거 그냥 피보나치문제였잖아,,,,, ㅡㅡ

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -21,91 +21,34 @@ public:
 
 Node nodes[201];
 int visit[201]{ 0, };
+int dp[2001];
 
 int N, M;
 
-int calcul(int start)
+
+int dfs(int curr, int depth)
 {
-	int count = 1;
-	visit[start] = true;
-	priority_queue<int> pq;
-
-	pq.push(start);
-
-	while (!pq.empty())
-	{
-		int t = pq.top();
-		pq.pop();
-
-		for (int i = 0; i < N; ++i)
-		{
-			int p = nodes[t].others[i];
-			if (p == 1 && visit[i] == false)
-			{
-				visit[i] = true;
-				nodes[start].others[i] = true;
-				nodes[i].others[start] = true;
-				count++;
-				pq.push(i);
-			}
-		}
-	}
-	return count;
+	if (curr == 0) return 1;
+	if (curr < 0) return 0;
+	if (dp[curr] != -1) return dp[curr];
+	
+	int a = dfs(curr - 1, depth + 1);
+	dp[curr] = max(dp[curr], a);
+	cout << a << endl;
+	int b = dfs(curr - 2, depth + 1);
+	dp[curr] = max(dp[curr], a + b);
+	return (a + b) % 1234567;
 }
+
 
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	unordered_map<int, int> trip;
-	cin >> N >> M;
-
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 0; j < N; ++j)
-		{
-			cin >> nodes[i].others[j];
-		}
-	}
-
-	for (int i = 0; i < M; ++i)
-	{
-		int k;
-		cin >> k;
-		trip[k - 1] = 1;
-	}
-
-	int count = 0;
-	for (int i = 0; i < N; ++i)
-	{
-		fill_n(visit, 201, 0);
-		if (visit[i] == 0)
-		{
-			count += calcul(i);
-		}
-		//if (count == N)
-		//	break;
-	}
-
-	for (int i = 0; i < N; ++i)
-	{
-		for (int j = 0; j < N; ++j)
-		{
-			if (i == j) continue;
-
-			if (nodes[i].others[j] == 0)
-			{
-				if (trip[i] == 1 && trip[j] == 1)
-				{
-					cout << "NO";	//여행못감
-					return 0;
-				}
-			}
-
-		}
-
-	}
-
-	cout << "YES";
+	cin >> N;
+	fill_n(dp, 1234568, -1);
+	int b = dfs(N, 0);
+	cout << b << endl;
 	return 0;
 }
