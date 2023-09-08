ac7123cba28658c96fd2b5dd29e6e4df6838e077
boy0501 <boy0501@naver.com>
2023-09-08 17:13:02 +0900

    백준 16432 골드4 떡장수와 호랑이 완
    dfs로 풀면 시간초과남.. ㅋㅋ;;
    어떻게어떻게 예외처리 해서 넘겼는데 dp로 푸는 문제였음 디용쓰 ?

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -14,55 +14,180 @@
 #include <functional>
 using namespace std;
 
-class Node {
-public:
-	vector<pair<int,int>> next;
-	Node(){};
-};
-#define inf 100000001
+int res[1001];
 
-class Button;
-typedef void (*test) (Button&);
+vector<vector<int>> d;
+int N;
 
-class Button {
-
-public:
-
-	void (*bfunc)(void);
-	test mfunc;
-	void link(test b)
+int dfs(int depth,int prev)
+{
+	if (depth == N)
 	{
-		mfunc = b;
+		return 0;
 	}
 
-	void onp()
-	{
-	}
-};
+	auto& vec = d[depth];
 
-class Dialog {
+	for (auto& val : vec)
+	{
+		
+		if (prev != val)
+		{
+			int k = dfs(depth + 1, val);
+			if (k == 0)
+			{
+				res[depth] = val;
+				return 0;
+			}else if (k != -1)
+			{
+				res[depth] = k;
+				return k;
+			}
+		}
+	}
 
-public:
-	Button mbt;
-	static void k(Button& b)
+	return -1;
+}
+
+int udfs(int depth, int prev)
+{
+	if (depth == -1)
 	{
-		cout << " dd" << endl;
+		return 0;
 	}
-	void pp()
+
+	auto& vec = d[depth];
+
+	for (auto& val : vec)
 	{
-		mbt.mfunc = k;
+
+		if (prev != val)
+		{
+			int k = udfs(depth - 1, val);
+			if (k == 0)
+			{
+				res[depth] = val;
+				return 0;
+			}
+			else if (k != -1)
+			{
+				res[depth] = k;
+				return k;
+			}
+		}
 	}
-};
+
+	return -1;
+}
+
+void gogo(int depth, int value)
+{
+	auto& vec = d[depth];
+
+	for (auto& v : vec)
+	{
+		if (v != value)
+		{
+			res[depth] = v;
+		}
+	}
+}
+
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	Dialog a{};
-	a.pp();
-	
-	a.mbt.mfunc();
+	cin >> N;
+	unordered_map<int, int> check;
+	vector<int> starts;
 
+	int flag = false;
+	for (int i = 0; i < N; ++i)
+	{
+		int dduck;
+		cin >> dduck;
+		vector<int> kindof;
+		if (dduck == 1)
+		{
+			starts.push_back(i);
+			check[i] = 1;
+		}
+		for (int j = 0; j < dduck; ++j)
+		{
+			int k;
+			cin >> k;
+			kindof.push_back(k);
+		}
+
+		d.push_back(kindof);
+	}
+	for (int i = 0; i < N; ++i)
+	{
+		for (auto& start : starts)
+		{
+			int mi = start - 1;
+			int ma = min(start + 1, N - 1);
+			auto& dma = d[ma];
+			if(mi != -1)
+				d[mi].erase(remove(d[mi].begin(), d[mi].end(), d[start][0]), d[mi].end());
+			if(ma != N -1)
+				dma.erase(remove(dma.begin(), dma.end(), d[start][0]), dma.end());
+		}
+
+		starts.clear();
+
+		for (int j = 0; j < d.size(); ++j)
+		{
+			if (d[j].size() == 0)
+			{
+				cout << -1 << endl;
+				return 0;
+			}
+			else if (d[j].size() == 1)
+			{
+				starts.push_back(j);
+			}
+		}
+
+		if (starts.size() > 1)
+		{
+			//1종류만 있는게 서로 붙어있는 경우 예외
+			int prev = starts[0];
+
+			for (int i = 1; i < starts.size(); ++i)
+			{
+				if (prev + 1 == starts[i])
+				{
+					if (d[prev] == d[starts[i]])
+					{
+						flag = true;
+					}
+				}
+
+				prev = starts[i];
+			}
+
+			if (flag)
+			{
+				cout << -1 << endl;
+				return 0;
+			}
+		}
+	}
+
+
+	dfs(0, 0);
+	if (res[N - 1] == 0) {
+		cout << -1 << endl;
+	}
+	else {
+
+		for (int i = 0; i < N; ++i)
+		{
+			cout << res[i] << endl;
+		}
+	}
 
 	return 0;
 }
