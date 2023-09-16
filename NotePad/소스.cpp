39d06623aa639cb979034fadce80fda22d2975fd
boy0501 <boy0501@naver.com>
2023-09-16 11:06:25 +0900

    1405번 미친로봇 골드4 완
    dfs길찾기 완벽탐색 백트래킹문제,,

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -14,93 +14,77 @@
 #include <functional>
 using namespace std;
 
-string str;
-
-int calcul(int front, int rear)
+int visit[35][35]{};
+double dir[4];
+double result;
+int N;
+double dfs(int cnt, int x,int y,double percent)
 {
-	int k = 1;
-	if (front + 3 <= rear)
+	if (cnt == N) {
+		result += percent;
+		return percent;
+	}
+	double k = 0.0;
+	if (visit[y+1][x] == false)
 	{
-		if (str[front + 1] == str[rear - 1])
+		if (dir[0] > 0.0)
 		{
-			k = calcul(front + 1, rear - 1);
-		}
-		else {
-			if (str[front + 1] < str[rear - 1])
-			{
-				return 1;
-			}
-			else if (str[front + 1] > str[rear - 1])
-			{
-				return 0;
-			}
+			visit[y + 1][x] = true;
+			k = dfs(cnt + 1, x, y + 1, percent * dir[0]);
+			visit[y + 1][x] = false;
 		}
 	}
+	if (visit[y - 1][x] == false)
+	{
+		if (dir[1] > 0.0)
+		{
+			visit[y - 1][x] = true;
+			k = dfs(cnt + 1, x, y - 1, percent * dir[1]);
+			visit[y - 1][x] = false;
+		}
+	}
+	if (visit[y][x + 1] == false)
+	{
+		if (dir[2] > 0.0)
+		{
+			visit[y][x + 1] = true;
+			k = dfs(cnt + 1, x + 1, y, percent * dir[2]);
+			visit[y][x + 1] = false;
+		}
+	}
+	if (visit[y][x - 1] == false)
+	{
+		if (dir[3] > 0.0)
+		{
+			visit[y][x - 1] = true;
+			k = dfs(cnt + 1, x - 1, y, percent * dir[3]);
+			visit[y][x - 1] = false;
+		}
+	}
+
 	return k;
 }
-
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	int N;
 	cin >> N;
-	for (int i = 0; i < N; ++i)
+	for (int i = 0; i < 4; ++i)
 	{
-		char t;
-		cin >> t;
-		str.push_back(t);
+		int k;
+		cin >> k;
+		dir[i] = k / 100.0;
 	}
 
-	int front = 0, rear = N - 1;
+	int sx = 15, sy = 15;
 
-	string result;
-	for (int i = 0; i < N; ++i)
-	{
-		if (str[front] < str[rear])
-		{
-			result.push_back(str[front]);
-			front++;
-		}
-		else if (str[front] > str[rear])
-		{
-			result.push_back(str[rear]);
-			rear--;
-		}
-		else {
-			if (calcul(front, rear))
-			{
-				//front º±≈√
-				result.push_back(str[front]);
-				front++;
-			}
-			else {
+	visit[sy][sx] = 1;
 
-				result.push_back(str[rear]);
-				rear--;
-			}
+	dfs(0, sx, sy, 1);
 
-		}
-	}
-
-	int flag = false;
-	for (int i = 0; i < result.size(); ++i)
-	{
-		cout << result[i];
-		if (i == 79) {
-			flag = true;
-			break;
-		}
-	}
-	if (flag)
-	{
-		for (int i = 80; i < result.size(); ++i)
-		{
-			if (i % 80 == 0) { cout << endl << result[i]; }
-			else	cout << result[i];
-		}
-	}
+	cout.precision(10);
+	cout << result;
 
 
 	return 0;
