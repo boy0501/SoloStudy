6c244c4553aeced39ff015e7565a3fd204c3e576
boy0501 <boy0501@naver.com>
2023-10-16 21:39:41 +0900

    CAS 실험

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -12,54 +12,23 @@
 #include <random>
 #include <algorithm>
 #include <functional>
+#include <chrono>
+#include <atomic>
 using namespace std;
-
-class Network {
-
-public:
-	Network() {};
-	virtual ~Network() {
-	
-		cout << "Network" << endl;
-	};
-	void discon() {};
-
-	void sendpp() { cout << " dd\n" << k; };
-
-
-	void func(void(Network::* fp)(void))
-	{
-		(*this.*fp)();
-	}
-
-protected:
-	int k = 7;
-};
-
-class Object {
-public:
-	Object() {};
-	virtual ~Object() {
-		cout << "Object" << endl;
-	};
-
-	void func(void(Network::* fp)(void),Network n)
-	{
-		(n.*fp)();
-	}
-
-protected:
-	int x, y, z;
-};
+using namespace chrono;
 
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-
-	Object o;
-	Network n;
-	o.func(&Network::sendpp, n);
-	n.func(&Network::sendpp);
+	bool expect = 0;
+	bool mGameEndFlag =true ;
+	if (std::atomic_compare_exchange_strong(reinterpret_cast<std::atomic_bool*>(&mGameEndFlag), &expect, 1))
+	{
+		cout << "dd";
+		return true;
+	}
+	return false; 
+	
 	return 0;
 }
