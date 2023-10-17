c588cae08211dd2595c833b14a3872a59cec68ad
boy0501 <boy0501@naver.com>
2023-10-17 22:17:22 +0900

    백준 1484 골드5 투포인터문제
    아니개쉬운건데 왜케못푸냐

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -18,107 +18,49 @@
 using namespace std;
 using namespace chrono;
 
-
+long long arr[50'100];
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
+	long long G;
+	cin >> G;
 
-	int N, M, L;
-	cin >> N >> M >> L;
-	vector<Node> hue;
-	multimap<pair<int, Node*>, Node*, Comp> res;
-	for (int i = 0; i < N; ++i)
+	vector<long long> killa;
+
+	for (long long i = 1; i < 50'100; ++i)
 	{
-		int t{};
-		cin >> t;
-		hue.push_back(Node(t, -1, -1));
+		arr[i] = i * i;
 	}
 
-	hue.push_back(Node(0, -1, -1));
-	hue.push_back(Node(L, -1, -1));
-
-	sort(hue.begin(), hue.end());
-	int minlen = 9999;
-	int maxlen = -1;
-	hue[0].nlen = hue[1].value - hue[0].value;
-	hue[0].cal();
-	for (int i = 1; i < hue.size() - 1; ++i)
+	vector<int> res;
+	int start = 1;
+	int end = 2;
+	while(true)
 	{
-		hue[i].nlen = hue[i + 1].value - hue[i].value;
-		hue[i].plen = hue[i].value - hue[i - 1].value;
-		hue[i].cal();
-		//minlen = min(minlen, hue[i].length);
-		//maxlen = max(maxlen, hue[i].length);
-	}
-	hue[hue.size() - 1].plen = hue[hue.size() - 1].value - hue[hue.size() - 2].value;
-	hue[hue.size() - 1].cal();
-
-
-
-	for (int i = 0; i < hue.size(); ++i)
-	{
-		res.insert(make_pair(make_pair(max(hue[i].plen, hue[i].nlen), &hue[i]), &hue[i]));
-	}
-	for (int i = 0; i < M; ++i)
-	{
-		Node* t1{}, * t2{};
-		t1 = res.begin()->second;
-		t2 = next(res.begin())->second;
-		bool flag = true;
-		if (t1->value > t2->value)
-			flag = false;
-
-		int nextlen = t1->highlen / 2;
-		int nextpos;
-		if (flag)
+		if (start == end) break;
+		if (end == 50001) break;
+		if (arr[end] - arr[start] < G)
 		{
-			nextpos = t1->value + nextlen;
-			t1->nlen = nextpos - t1->value;
-			t2->plen = t2->value - nextpos;
-			t1->cal();
-			t2->cal();
-			res.erase(next(res.begin()));
-			res.erase(res.begin());
-
-			auto insert1 = new Node(t1->value, t1->plen, t1->nlen);
-			auto insert2 = new Node(t2->value, t2->plen, t2->nlen);
-			auto insert3 = new Node(nextpos, t1->nlen, t2->plen);
-			res.insert(make_pair(make_pair(insert1->highlen, insert1), insert1));
-			res.insert(make_pair(make_pair(insert2->highlen, insert2), insert2));
-			res.insert(make_pair(make_pair(insert3->highlen, insert3), insert3));
+			end++;
 		}
-		else
+		else if(arr[end] - arr[start] > G ){
+			start++;
+		}
+		else if (arr[end] - arr[start] == G)
 		{
-			//nextpos = t1->value - nextlen;
-			//t1->plen = t1->value - nextpos;
-			//t2->nlen = nextpos - t2->value;
-			//t1->cal();
-			//t2->cal();
-			//res.erase(next(res.begin()));
-			//res.erase(res.begin());
-			//
-			//
-			//auto insert1 = new Node(t1->value, t1->plen, t1->nlen);
-			//auto insert2 = new Node(t2->value, t2->plen, t2->nlen);
-			//auto insert3 = new Node(nextpos, nextlen, nextlen);
-			//res.insert(make_pair(make_pair(t1->highlen, insert1), insert1));
-			//res.insert(make_pair(make_pair(t2->highlen, insert2), insert2));
-			//res.insert(make_pair(make_pair(nextlen, insert3), insert3));
+			res.push_back(end );
+			end++;
 		}
 	}
-
-	
-
-	cout << res.begin()->first.first << endl;
-	
-
+	if (res.empty())
+	{
+		cout << -1 << endl;
+		return 0;
+	}
+	for (auto p : res)
+	{
+		cout << p << endl;
+	}
 	return 0;
 }
-
-/*
-
-6 7 800
-622 411 201 555 755 82
-
-*/
