191be4b09605703d44feefcdbac99067493fdfc2
boy0501 <boy0501@naver.com>
2023-10-19 17:26:41 +0900

    10026번 골드5 적록색약 완 ㅡㅡ
    아니 fiill_n 잘못써서 계속 틀림. 걍 쓰지말자이거;

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -18,85 +18,112 @@
 using namespace std;
 using namespace chrono;
 
-class Node {
-public:
-	int visit;
-	int StackPointer = -1;
-	int cnt = -1;
-};
-Node node[1'000'00];
+int greed[120][120];
+int grgreed[120][120];
+int visit[120][120];
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,-1,1 };
+
+void dfsgreed(int x, int y, int color)
+{
+	if (visit[y][x] == -1) return;
+	if (visit[y][x] == 1) return;
+	if (greed[y][x] != color) return;
+	visit[y][x] = 1;
+	for (int i = 0; i < 4; ++i)
+	{
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if(visit[ny][nx] == 0)
+			dfsgreed(x + dx[i], y + dy[i],color);
+	}
+	return;
+}
+
+void dfsgrgreed(int x, int y, int color)
+{
+	if (visit[y][x] == -1) return;
+	if (visit[y][x] == 1) return;
+	if (grgreed[y][x] != color) return;
+	visit[y][x] = 1;
+	for (int i = 0; i < 4; ++i)
+	{
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (visit[ny][nx] == 0)
+			dfsgrgreed(x + dx[i], y + dy[i], color);
+	}
+	return;
+}
+
+void init(int N)
+{
+	for (int i = 0; i < 120; ++i)
+	{
+		fill_n(visit[i], sizeof(visit[i]), -1);
+	}
+	for (int i = 1; i <= N; ++i)
+	{
+		for (int j = 1; j <= N; ++j)
+		{
+			visit[i][j] = 0;
+		}
+	}
+	return;
+}
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-	
-	string str;
-	string boom;
 
-	cin >> str;
-	cin >> boom;
-
-	deque<pair<char,int>> res;
+	int N;
+	cin >> N;
+	init(N);
+	for (int i = 1; i <= N; ++i)
+	{
+		for (int j = 1; j <= N; ++j)
+		{
+			char color;
+			cin >> color;
+			greed[i][j] = color;
+			if (color == 'R' || color == 'G')
+				grgreed[i][j] = 'R';
+			else
+				grgreed[i][j] = color;
+		}
+	}
 
 	int cnt = 0;
-	int i = 0;
-	for(int i = 0 ; i < str.size() ;++i)
+	int grcnt = 0;
+
+	for (int i = 1; i <= N; ++i)
 	{
-		if (str[i] == boom[cnt])
+		for (int j = 1; j <= N; ++j)
 		{
-			cnt++;
-			res.push_back(make_pair(str[i], cnt));
-			
-			if (cnt == boom.size())
+			if (visit[i][j] == 0)
 			{
-				for (int j = 0; j < boom.size(); ++j)
-				{
-					res.pop_back();
-				}
-				if (!res.empty())
-					cnt = res.back().second;
-				else
-					cnt = 0;
-			}		
-		
-		}
-		else {
-			if (str[i] == boom[0])
-			{
-				cnt = 1;
-				res.push_back(make_pair(str[i], cnt));
-
-				if (cnt == boom.size())
-				{
-					for (int j = 0; j < boom.size(); ++j)
-					{
-						res.pop_back();
-						cnt--;
-					}
-					if (!res.empty())
-						cnt = res.back().second;
-					else
-						cnt = 0;
-				}
+				dfsgreed(j, i, greed[i][j]);
+				cnt++;
 			}
-			else {
-				cnt = 0;
-				res.push_back(make_pair(str[i], cnt));
-
+		}
+	}
+	init(N);
+	for (int i = 1; i <= N; ++i)
+	{
+		for (int j = 1; j <= N; ++j)
+		{
+			if (visit[i][j] == 0)
+			{
+				dfsgrgreed(j, i, grgreed[i][j]);
+				grcnt++;
 			}
 		}
 	}
 
-	if (res.empty())
-	{
-		cout << "FRULA";
-	}
-	else {
-		for (auto& p : res)
-		{
-			cout << p.first;
-		}
-	}
+	cout << cnt << " " << grcnt;
 
+
+	
 	return 0;
 }
