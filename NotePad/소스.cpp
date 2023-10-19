9b9e409c5230df708ac6766336597fb39245aee4
boy0501 <boy0501@naver.com>
2023-10-20 00:13:39 +0900

    17140 골드 2 이차원배열과 연산 솔
    단순구현문제. 이정도만 나와도 푼다

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -17,113 +17,222 @@
 #include <queue>
 using namespace std;
 using namespace chrono;
+constexpr int FAILCODE = 9999999;
+deque<deque<int>> arrs;
 
-int greed[120][120];
-int grgreed[120][120];
-int visit[120][120];
+int r, c, k;
 
-int dx[4] = { 1,-1,0,0 };
-int dy[4] = { 0,0,-1,1 };
-
-void dfsgreed(int x, int y, int color)
+int dfs(int maxRow, int maxCol, int cnt, char order, deque<deque<int>> arr)
 {
-	if (visit[y][x] == -1) return;
-	if (visit[y][x] == 1) return;
-	if (greed[y][x] != color) return;
-	visit[y][x] = 1;
-	for (int i = 0; i < 4; ++i)
+	int res = FAILCODE;
+	if (cnt > 101) return FAILCODE;
+	if (maxRow >= r && maxCol >= c)
 	{
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if(visit[ny][nx] == 0)
-			dfsgreed(x + dx[i], y + dy[i],color);
-	}
-	return;
-}
-
-void dfsgrgreed(int x, int y, int color)
-{
-	if (visit[y][x] == -1) return;
-	if (visit[y][x] == 1) return;
-	if (grgreed[y][x] != color) return;
-	visit[y][x] = 1;
-	for (int i = 0; i < 4; ++i)
-	{
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-		if (visit[ny][nx] == 0)
-			dfsgrgreed(x + dx[i], y + dy[i], color);
-	}
-	return;
-}
-
-void init(int N)
-{
-	for (int i = 0; i < 120; ++i)
-	{
-		fill_n(visit[i], sizeof(visit[i]), -1);
-	}
-	for (int i = 1; i <= N; ++i)
-	{
-		for (int j = 1; j <= N; ++j)
+		if (order == 'R')
 		{
-			visit[i][j] = 0;
+			if (arr[r - 1][c - 1] == k)
+				return cnt - 1;
+		}
+		else
+		{
+			if (arr[c - 1][r - 1] == k)
+				return cnt - 1;
+		}
+
+	}
+	if (order == 'R')
+	{
+		for (int i = 0; i < maxRow; ++i)
+		{
+			sort(begin(arr[i]), end(arr[i]));
+			vector<pair<int, int>> vec(110);
+			for (auto& p : arr[i])
+			{
+				vec[p] = make_pair(p, vec[p].second + 1);
+			}
+
+			sort(vec.begin(), vec.end(), [](pair<int, int> a, pair<int, int> b) {
+				if (a.second == b.second)
+				{
+					return a.first < b.first;
+				}
+				return a.second < b.second;
+				});
+
+			deque<int> narr;
+			for (int i = 0; i < vec.size(); ++i)
+			{
+				if (vec[i].first == 0) continue;
+				narr.push_back(vec[i].first);
+				narr.push_back(vec[i].second);
+			}
+			arr[i].clear();
+			arr[i] = narr;
+		}
+		int nmax = 0;
+		for (int i = 0; i < maxRow; ++i)
+		{
+			nmax = max(nmax, (int)arr[i].size());
+		}
+
+		for (int i = 0; i < maxRow; ++i)
+		{
+			if (arr[i].size() != nmax)
+			{
+				int asize = arr[i].size();
+				for (int j = 0; j < nmax - asize; ++j)
+					arr[i].push_back(0);
+			}
+		}
+
+		int 열의개수 = nmax;
+		int 행의개수 = arr.size();
+		if (행의개수 >= 열의개수)
+		{
+			res = dfs(행의개수, 열의개수, cnt + 1, 'R', arr);
+		}
+		else {
+			deque<deque<int>> narr;
+
+			for (int i = 0; i < nmax; ++i)
+			{
+				deque<int> tmp;
+				narr.push_back(tmp);
+				for (int j = 0; j < 행의개수; ++j)
+				{
+					narr[i].push_back(arr[j][i]);
+				}
+			}
+
+			res = min(res, dfs(행의개수, 열의개수, cnt + 1, 'C', narr));
+
+		}
+
+	}
+	else if (order == 'C')
+	{
+		for (int i = 0; i < maxCol; ++i)
+		{
+			sort(begin(arr[i]), end(arr[i]));
+			vector<pair<int, int>> vec(110);
+			for (auto& p : arr[i])
+			{
+				vec[p] = make_pair(p, vec[p].second + 1);
+			}
+
+			sort(vec.begin(), vec.end(), [](pair<int, int> a, pair<int, int> b) {
+				if (a.second == b.second)
+				{
+					return a.first < b.first;
+				}
+				return a.second < b.second;
+				});
+
+			arr[i].clear();
+			deque<int> narr;
+			for (int i = 0; i < vec.size(); ++i)
+			{
+				if (vec[i].first == 0) continue;
+				narr.push_back(vec[i].first);
+				narr.push_back(vec[i].second);
+			}
+			arr[i] = narr;
+		}
+		int nmax = 0;
+		for (int i = 0; i < maxCol; ++i)
+		{
+			nmax = max(nmax, (int)arr[i].size());
+		}
+
+		int 행의개수 = nmax;
+		int 열의개수;
+		for (int i = 0; i < maxCol; ++i)
+		{
+			if (arr[i].size() != 행의개수)
+			{
+				int asize = arr[i].size();
+				for (int j = 0; j < 행의개수 - asize; ++j)
+					arr[i].push_back(0);
+			}
+		}
+		열의개수 = arr.size();
+		if (행의개수 >= 열의개수)
+		{
+			deque<deque<int>> narr;
+
+			for (int i = 0; i < nmax; ++i)
+			{
+				deque<int> tmp;
+				narr.push_back(tmp);
+				for (int j = 0; j < maxCol; ++j)
+				{
+					narr[i].push_back(arr[j][i]);
+				}
+			}
+			res = dfs(행의개수, 열의개수, cnt + 1, 'R', narr);
+		}
+		else {
+
+
+			res = min(res, dfs(행의개수, 열의개수, cnt + 1, 'C', arr));
+
 		}
 	}
-	return;
+	return res;
 }
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 
-	int N;
-	cin >> N;
-	init(N);
-	for (int i = 1; i <= N; ++i)
+
+	cin >> r >> c >> k;
+
+	for (int i = 0; i < 3; ++i)
 	{
-		for (int j = 1; j <= N; ++j)
+		deque<int> tmp;
+		arrs.push_back(tmp);
+		for (int j = 0; j < 3; ++j)
 		{
-			char color;
-			cin >> color;
-			greed[i][j] = color;
-			if (color == 'R' || color == 'G')
-				grgreed[i][j] = 'R';
-			else
-				grgreed[i][j] = color;
+			int k;
+			cin >> k;
+
+			arrs[i].push_back(k);
 		}
 	}
 
-	int cnt = 0;
-	int grcnt = 0;
-
-	for (int i = 1; i <= N; ++i)
+	if (3 >= r && 3 >= c)
 	{
-		for (int j = 1; j <= N; ++j)
+
+		if (arrs[r - 1][c - 1] == k)
 		{
-			if (visit[i][j] == 0)
+			cout << "0";
+		}
+		else {
+			int res = dfs(3, 3, 1, 'R', arrs);
+			if (res == FAILCODE)
 			{
-				dfsgreed(j, i, greed[i][j]);
-				cnt++;
+				cout << -1;
+			}
+			else {
+				cout << res;
 			}
 		}
+
 	}
-	init(N);
-	for (int i = 1; i <= N; ++i)
-	{
-		for (int j = 1; j <= N; ++j)
+	else {
+		int res = dfs(3, 3, 1, 'R', arrs);
+		if (res == FAILCODE)
 		{
-			if (visit[i][j] == 0)
-			{
-				dfsgrgreed(j, i, grgreed[i][j]);
-				grcnt++;
-			}
+			cout << -1;
+		}
+		else {
+			cout << res;
 		}
 	}
 
-	cout << cnt << " " << grcnt;
 
 
-	
+
 	return 0;
 }
