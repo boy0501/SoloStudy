5d3157d18da9592f2387df0ec42e40e6faef9abd
boy0501 <boy0501@naver.com>
2023-10-22 02:39:31 +0900

    프로그래머스 추석 트래픽 3 *n^2 솔브

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -18,306 +18,110 @@
 using namespace std;
 using namespace chrono;
 constexpr int FAILCODE = 9999999;
-deque<deque<int>> arrs;
-//
-//int n, m;
-//int indexing;
-//int visit[7][7];
-//int arr[7][7];
-///*
-//shape는 총 4개
-//1 - ┌
-//2 - ㄱ
-//3 - ㄴ
-//4 - ┘
-//*/
 
-//
-//int aa(int index, int shape)
-//{
-//	if (index == n * m ) return 0;
-//
-//	int y = index / indexing + 1;
-//	int x = index % indexing + 1;
-//	int res = 0;
-//
-//	switch (shape)
-//	{
-//	case 1:
-//		if (visit[y + 1][x] == -1 || visit[y + 1][x] == 1) return 0;
-//		if (visit[y][x + 1] == -1 || visit[y][x + 1] == 1) return 0;
-//		break;
-//	case 2:
-//		if (visit[y][x - 1] == -1 || visit[y][x - 1] == 1) return 0;
-//		if (visit[y + 1][x] == -1 || visit[y + 1][x] == 1) return 0;
-//		break;
-//	case 3:
-//		if (visit[y - 1][x] == -1 || visit[y - 1][x] == 1) return 0;
-//		if (visit[y][x + 1] == -1 || visit[y][x + 1] == 1) return 0;
-//		break;
-//	case 4:
-//		if (visit[y - 1][x] == -1 || visit[y - 1][x] == 1) return 0;
-//		if (visit[y][x - 1] == -1 || visit[y][x - 1] == 1) return 0;
-//		break;
-//	}
-//	res += arr[y][x] * 2;
-//	switch (shape)
-//	{
-//	case 1:
-//		visit[y + 1][x] = true;
-//		visit[y][x + 1] = true;
-//		res += arr[y + 1][x];
-//		res += arr[y][x + 1];
-//		break;
-//	case 2:
-//		visit[y][x - 1] = true;
-//		visit[y + 1][x] = true;
-//		res += arr[y][x - 1];
-//		res += arr[y + 1][x];
-//		break;
-//	case 3:
-//		visit[y - 1][x] = true;
-//		visit[y][x + 1] = true;
-//		res += arr[y - 1][x];
-//		res += arr[y][x + 1];
-//		break;
-//	case 4:
-//		visit[y - 1][x] = true;
-//		visit[y][x - 1] = true;
-//		res += arr[y - 1][x];
-//		res += arr[y][x - 1];
-//		break;
-//	}
-//	
-//	int dfsret = 0;
-//
-//	for (int i = index + 1; i < n * m; ++i)
-//	{
-//		int ny = i / indexing + 1;
-//		int nx = i % indexing + 1;
-//		if (visit[ny][nx] == true) continue;
-//		int ret1 = 0;
-//		visit[ny][nx] = true;
-//		ret1 = dfs(i, 1);
-//		visit[ny][nx] = false;
-//		if (ret1 != 0)
-//		{
-//			if (visit[ny + 1][nx] == true)
-//				visit[ny + 1][nx] = false;
-//			if (visit[ny][nx + 1] == true)
-//				visit[ny][nx + 1] = false;
-//		}
-//
-//		int ret2 = 0;
-//
-//		visit[ny][nx] = true;
-//		ret2 = dfs(i, 2);
-//		visit[ny][nx] = false;
-//		if (ret2 != 0)
-//		{
-//			if (visit[ny][nx - 1] == true)
-//				visit[ny][nx - 1] = false;
-//			if (visit[ny + 1][nx] == true)
-//				visit[ny + 1][nx] = false;
-//		}
-//
-//
-//		int ret3 = 0;
-//		visit[ny][nx] = true;
-//		ret3 = dfs(i, 3);
-//		visit[ny][nx] = false;
-//		if (ret3 != 0)
-//		{
-//			if (visit[ny - 1][nx] == true)
-//				visit[ny - 1][nx] = false;
-//			if (visit[ny][nx + 1] == true)
-//				visit[ny][nx + 1] = false;
-//		}
-//
-//
-//		int ret4 = 0;
-//		visit[ny][nx] = true;
-//		ret4 = dfs(i, 4);
-//		visit[ny][nx] = false;
-//		if (ret4 != 0)
-//		{
-//			if (visit[ny - 1][nx] == true)
-//				visit[ny - 1][nx] = false;
-//			if (visit[ny][nx - 1] == true)
-//				visit[ny][nx - 1] = false;
-//		}
-//
-//		dfsret = max(dfsret,max(ret1, max(ret2, max(ret3, ret4))));
-//		if (dfsret == 24)
-//			break;
-//	}
-//
-//
-//	return res + dfsret;
-//
-//}
+int solution(vector<string> lines) {
+	int answer = 0;
 
+	vector<pair<int, int>> logs;
 
-
-int visit[3][100'10];
-int arr[3][100'10];
-int goal = 0;
-int minim = FAILCODE;
-int dfs(int cnt, int lane, int index,int obs[3])
-{
-	if (index == goal)
+	for (auto& line : lines)
 	{
-		return cnt;
+		size_t sz;
+		int hour = stoi(line.substr(11, 2), &sz, 10);
+		int min = stoi(line.substr(14, 2), &sz, 10);
+		int sec = stoi(line.substr(17, 2), &sz, 10);
+		int milli = stoi(line.substr(20, 3), &sz, 10);
+		int durtime = stoi(line.substr(24), &sz, 10);
+		int durmilli = 0;
+		if (line[25] == '.')
+		{
+			int k = 100;
+			for (int i = 26; i < line.size(); ++i)
+			{
+				if (line[i] == 's') break;
+				durmilli += atoi(line.substr(i,1).c_str()) * k;
+				k /= 10;
+			}
+		}
+		int wholemilli = (hour * 3600 + min * 60 + sec) * 1000 + milli;
+		logs.push_back(make_pair(wholemilli, durtime * 1000 + durmilli));
 	}
-	if (obs[lane] <= 0) {
-		return cnt;
-	}
-	if (cnt > minim)
+
+	int start = logs[0].first - logs[0].second + 1;
+	int selectEnd = logs[0].first ;
+	bool flag = false;
+	for (int i = 0; i < logs.size();)
 	{
-		return FAILCODE;
-	}
-	int res = FAILCODE;
-	int nx = index;
-
-	if (arr[lane][nx + 1] == true)
-	{
-		if (cnt + 1 > minim)
+		int counts = 1;
+		int endtime = start + 999;
+		if (flag) break;
+		flag = true;
+		int nextleveltrigger = false;
+		for (int j = i + 1; j < logs.size(); ++j)
 		{
-			return FAILCODE;
+			int nstart = logs[j].first - logs[j].second + 1;
+			int nend = logs[j].first;
+
+			if (nstart <= endtime)
+			{
+				counts++;
+			}
+			else {
+				flag = false;
+				if (!nextleveltrigger)
+				{
+					int nextstart = start + 1000;
+					if (start == selectEnd)
+					{
+						//검사결과가 내 마지막부근이였다면 다음블럭으로
+						++i;
+						start = logs[i].first - logs[i].second + 1;
+						selectEnd = logs[i].first;
+					}
+					else {
+						if (nextstart <= selectEnd)
+						{
+							start = nextstart; // 내 블럭이 1초 이상에도 있을 때
+						}
+						else {
+							start = selectEnd;// 내 블럭이 1초 이상에 없을 때 내 마지막을 걸음
+						}
+
+					}
+					nextleveltrigger = true;
+				}
+
+
+
+				//break;
+			}
 		}
-		obs[lane]--;
-		int ret1 = FAILCODE, ret2 = FAILCODE, ret3 = FAILCODE;
-		if (lane == 0)
-		{
-			visit[1][nx + 1] = true;
-			ret1 = dfs(cnt + 1, 1, index + 1, obs);
-			visit[1][nx + 1] = false;
+		answer = max(answer, counts);
 
-			visit[2][nx + 1] = true;
-			ret1 = min(ret1, dfs(cnt + 1, 2, index + 1, obs));
-			visit[2][nx + 1] = false;
-		}
-		else if (lane == 1)
-		{
-			visit[0][nx + 1] = true;
-			ret2 = dfs(cnt + 1, 0, index + 1, obs);
-			visit[0][nx + 1] = false;
-
-			visit[2][nx + 1] = true;
-			ret2 = min(ret2, dfs(cnt + 1, 2, index + 1, obs));
-			visit[2][nx + 1] = false;
-
-		}
-		else if (lane == 2)
-		{
-			visit[0][nx + 1] = true;
-			ret3 = dfs(cnt + 1, 0, index + 1, obs);
-			visit[0][nx + 1] = false;
-
-			visit[1][nx + 1] = true;
-			ret3 = min(ret3, dfs(cnt + 1, 1, index + 1, obs));
-			visit[1][nx + 1] = false;
-
-		}
-		res = min(res, min(ret1, min(ret2, ret3)));
-		minim = min(minim, res);
-		obs[lane]++;
-	}
-	else {
-		visit[lane][nx + 1] = true;
-		res = min(res, dfs(cnt, lane, index + 1, obs));
-		visit[lane][nx + 1] = false;
 	}
 
-	return res;
+
+
+
+	return answer;
 }
 
-class Obs {
-public:
-	int lane;
-	int obstacle = 0;
-	int far = 99999;
-};
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
-	vector<int > obstacleLanes;
+
+	vector<string> lines;
 	int n;
 	cin >> n;
+	cin.ignore(199, '\n');
 	for (int i = 0; i < n; ++i)
 	{
-		int k = 0;
-		cin >> k;
-		obstacleLanes.push_back(k);
+		char tmp;
+		string str;
+		getline(cin, str);
+		lines.push_back(str);
 	}
-	int res = 0;
-	goal = obstacleLanes.size();
-
-	//pair<int, int> obss[3]{ {0,FAILCODE },{0,FAILCODE},{0,FAILCODE} };
-	int obs[3]{};
-	Obs obss[3];
-	obss[0].lane = 0;
-	obss[1].lane = 1;
-	obss[2].lane = 2;
-
-	int nowlane = 1;
-	int nowcnt = 0;
-	while(true)
-	{
-		obss[0].lane = 0;
-		obss[0].far = FAILCODE;
-		obss[0].obstacle = 0;
-		obss[1].lane = 1;
-		obss[1].far = FAILCODE;
-		obss[1].obstacle = 0;
-		obss[2].lane = 2;
-		obss[2].far = FAILCODE;
-		obss[2].obstacle = 0;
-		
-		for (int i = nowcnt; i < obstacleLanes.size(); ++i)
-		{
-
-			arr[obstacleLanes[i] - 1][i + 1] = true;
-			obs[obstacleLanes[i] - 1]++;
-			obss[obstacleLanes[i] - 1].obstacle++;
-			obss[obstacleLanes[i] - 1].far = min(obss[obstacleLanes[i] - 1].far, i + 1);
-		}
-
-		sort(begin(obss), end(obss), [](Obs a, Obs b) {
-			if (a.obstacle == b.obstacle)
-				return a.far > b.far;
-			return a.obstacle < b.obstacle;
-			});
-
-		if (nowlane == obss[0].lane)
-		{
-			if (obss[0].obstacle > 0)
-			{
-				nowlane = obss[1].lane;
-				nowcnt += obss[1].far;
-				res++;
-			}
-			else {
-
-				nowlane = obss[0].lane;
-				nowcnt += obss[0].far;
-			}
-		}
-		else {
-			nowlane = obss[0].lane;
-			nowcnt += obss[0].far;
-			res++;
-
-		}
-		if (nowcnt >= obstacleLanes.size())
-			break;
-	}
-	
-	
-
-
-	//res = dfs(0, 1, 0, obs);
-	std::cout << res << endl;
+	cout << solution(lines);
 	return 0;
 }
