bd06daaf83d94906e7223f0e03dfadacb6cbbb8f
boy0501 <37091845+boy0501@users.noreply.github.com>
2023-10-25 19:25:00 +0900

    [Gold V] Title: 합분해, Time: 0 ms, Memory: 2340 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.cc
@@ -0,0 +1,44 @@
+// main.cpp
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <cstring>
+#include <string>
+#include <stack>
+#include <map>
+#include <cmath>
+#include <random>
+#include <algorithm>
+#include <functional>
+#include <chrono>
+#include <atomic>
+#include <queue>
+using namespace std;
+using namespace chrono;
+constexpr int FAILCODE = 9999999;
+
+
+long long tabul[202][202]{};
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	long long N, K;
+	cin >> N >> K;
+	fill_n(tabul[0], 202, 1);
+	for (long long i = 1; i <= K; ++i)
+	{
+		tabul[i][0] = 1;
+		for (long long j = 1; j <= N; ++j)
+		{
+			tabul[i][j] = (tabul[i][j - 1] % 1'000'000'000 + tabul[i - 1][j] % 1'000'000'000) % 1'000'000'000;
+		}
+	}
+
+	cout << tabul[K -1][N];
+	return 0;
+}
