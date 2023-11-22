bd4d8ec084733688045cfc4f4d3b4364e2fc40f0
boy0501 <boy0501@naver.com>
2023-11-22 19:34:28 +0900

    Update 소스.cpp

--- NotePad/소스.cpp
+++ NotePad/소스.cpp
@@ -25,82 +25,39 @@ constexpr int FAILCODE = 9999999;
 int visits[101][71];
 int maps[101][71];
 
+class Node {
+public:
+	Node(int a, int b) :a(a), b(b) {}
+	int a;
+	int b;
+};
+struct Comp {
+	bool operator ()(const Node& a, const Node& b)
+	{
+		if (a.a == b.a)
+		{
+			return a.b > b.b;
+		}
+		return a.a < b.a;
+	}
+};
 int main()
 {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(NULL);
 	fill_n(visits[0], 101 * 71, -1);
-	int N, M;
-	cin >> N >> M;
-	for (int i = 1; i <= N; ++i)
+	priority_queue<Node,vector<Node>,Comp> pq;
+
+	pq.push(Node(1, 2));
+	pq.push(Node(7, 2));
+	pq.push(Node(4, 2));
+	pq.push(Node(4, 4));
+
+	while (!pq.empty())
 	{
-		for (int j = 1; j <= M; ++j)
-		{
-			int k;
-			cin >> k;
-			visits[i][j] = 0;
-			maps[i][j] = k;
-		}
+		auto p = pq.top();
+		pq.pop();
+		cout << p.a <<", "<<p.b<< endl;
 	}
-
-	int dx[8]{ 1,0,-1,0,-1,1,-1,1 };
-	int dy[8]{ 0,1,0,-1,-1,1,1,-1 };
-	queue<tuple<int,int,int>> q;
-	int TopCnt = 0;
-	for (int i = 1; i <= N; ++i)
-	{
-		for (int j = 1; j <= M; ++j)
-		{
-			if (maps[i][j] == 0)
-				continue;
-			if (visits[i][j] == true)
-				continue;
-			int isTop = true;
-			int CurrHeight = maps[i][j];
-			q.push(make_tuple(maps[i][j], j, i));
-			visits[i][j] = true;
-			while (!q.empty())
-			{
-
-				auto next = q.front();
-				q.pop();
-				int value = get<0>(next);
-				int x = get<1>(next);
-				int y = get<2>(next);
-				for (int k = 0; k < 8; ++k)
-				{
-					int nx = x + dx[k];
-					int ny = y + dy[k];
-					if (nx < 1) continue;
-					if (ny < 1) continue;
-					if (ny > N) continue;
-					if (nx > M) continue;
-					if (maps[ny][nx] > CurrHeight)
-					{
-						isTop = false;
-					}
-					if (visits[ny][nx] == true)
-						continue;
-					if (maps[ny][nx] == 0)
-						continue;
-
-					if (maps[ny][nx] == CurrHeight) {
-						visits[ny][nx] = true;
-						q.push(make_tuple(maps[ny][nx], nx, ny));
-					}
-					else if (maps[ny][nx] > CurrHeight)
-					{
-						isTop = false;
-					}
-				}
-			}
-			if (isTop)
-				TopCnt++;
-		}
-	}
-	
-
-	cout << TopCnt << endl;
-
 	return 0;
 }
